7164f02bb9eac499eb60990f18d2129a998822ab
Jonathan LAURENT <johjo@ytreza.org>
2023-10-25 15:59:18 +0200

    tell when too many tokens taken

--- domain/commands/stake_different_tokens_command.py
+++ domain/commands/stake_different_tokens_command.py
@@ -5,6 +5,7 @@ class TakeDifferentTokensCommand:
     def execute(self, blue, green, red, black, white):
         board = self.game_repository.get_game()
         player = board.players[0]
+        quantity = 0
 
         tokens = [("red", red), ("blue", blue), ("green", green), ("black", black), ("white", white) ]
 
@@ -12,5 +13,13 @@ class TakeDifferentTokensCommand:
             if taken:
                 board.stock.decrease(color, quantity=1)
                 player.stock.increase(color, quantity=1)
+                quantity += 1
+
+        if quantity == 4:
+            raise TooManyTokensTakenException()
 
         self.game_repository.save(board)
+
+
+class TooManyTokensTakenException(Exception):
+    pass
--- tests/test_take_different_tokens_command.py
+++ tests/test_take_different_tokens_command.py
@@ -1,7 +1,7 @@
 import pytest
 
 from adapters.game_repository_in_memory import GameRepositoryInMemory
-from domain.commands.stake_different_tokens_command import TakeDifferentTokensCommand
+from domain.commands.stake_different_tokens_command import TakeDifferentTokensCommand, TooManyTokensTakenException
 from tests.builder import StockBuilder, PlayerBuilder, BoardBuilder
 
 def a_stock(red, green, black, blue, white):
@@ -46,3 +46,12 @@ def test_should_first_player_take_token(tokens, expected):
     assert actual == expected.build()
 
 
+def test_should_tell_when_take_too_many_tokens():
+    # given
+    game_repository = GameRepositoryInMemory()
+    game_repository.feed(a_game().starting_for_two_players().build())
+
+    # when / then
+    with pytest.raises(TooManyTokensTakenException):
+        command = TakeDifferentTokensCommand(game_repository=game_repository)
+        command.execute(red=True, green=True, black=True, white=True, blue=False)
