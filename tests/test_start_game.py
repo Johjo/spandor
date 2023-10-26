9023a573f6f01c24406b598544bb1f375a77f0e7
Jonathan LAURENT <johjo@ytreza.org>
2023-10-26 11:57:35 +0200

    refactoring - fusion des tests

--- tests/test_start_game.py
+++ tests/test_start_game.py
@@ -37,23 +37,5 @@ def test_should_start_game(number_of_players, expected):
     assert actual == expected
 
 
-def test_should_start_game_for_three_players():
-    game_repository = GameRepositoryInMemory()
-    cards_repository = CardRepositoryStubbed()
-
-    level_1_cards = [a_card().with_id(uuid.uuid4()) for _ in range(4)]
-    cards_repository.feed(level=1, quantity=4, cards=level_1_cards)
-
-    start_game_command = StartGameCommand(game_repository=game_repository, cards_repository=cards_repository)
-    start_game_command.execute(number_of_players=3)
-
-    actual = game_repository.get_game()
-    expected = BoardBuilder()\
-                    .starting_for_three_players()\
-                    .with_cards_level_1(cards=level_1_cards)\
-                    .build()
-    assert actual == expected
-
-
 def a_card():
     return CardBuilder()
